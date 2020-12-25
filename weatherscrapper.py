b16eeb2c65ed7e94a1e41565f22f46a1f46b0192
NikolaJarakovic <70145279+NikolaJarakovic@users.noreply.github.com>
2020-12-25 19:08:18 +0100

    Update weatherscrapper.py
    Removed and modified  comments made in Serbian.
    Added some comments loosely explaining the code

--- weatherscrapper.py
+++ weatherscrapper.py
@@ -2,11 +2,12 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-
+#URL of the webpage i want to collect data from
 page = requests.get('https://forecast.weather.gov/MapClick.php?lat=41.8843&lon=-87.6324')
 soup = BeautifulSoup(page.content, 'html.parser')
+#Finding the data
 week = soup.find(id='seven-day-forecast-body')
-
+#Selecting and separating the daily data into a list
 items = week.find_all(class_='tombstone-container')
 
 items[0].find(class_='period-name').get_text()
@@ -14,24 +15,21 @@ items[0].find(class_='short-desc').get_text()
 
 short_descriptions = []
 period_names = []
-temperatures = []#ako nema na prvom onom mora jedan element ovde da se doda
-				#i da preskocis prvu iteraciju u for za temp
+temperatures = []
 
+#Filling in my data into dedicated lists
 for item in items:
 	period_names.append(item.find(class_='period-name').get_text())
-#print(period_names)
 
 for item in items:
 	short_descriptions.append(item.find(class_='short-desc').get_text())
-#print(short_descriptions)
 
 for item in items:
 	temperatures.append(item.find(class_='temp').get_text())
-#print(temperatures)
-
-#moze i preko list comprehensiona
-#period_names = [item.find(class_='period-name').get_text() for item in items]
-
+'''
+could've done it with list comprehension
+period_names = [item.find(class_='period-name').get_text() for item in items]
+'''
 weather_stuff = pd.DataFrame(
 	{'period': period_names,
 	'short_descriptions': short_descriptions,
